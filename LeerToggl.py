9dc7f817fabbccdbd25ac64daf34bd09bb048803
pacoalcaide <pacoalcaide@gmail.com>
2024-03-07 22:13:35 +0100

    arraglar indentaciones

--- LeerToggl.py
+++ LeerToggl.py
@@ -12,67 +12,66 @@ def get_entornos():
         entornos = json.load(fichero_entorno)
 
     return entornos
-  
-def main():
-  # variables generales
-  hoy_cadena = datetime.today().strftime("%Y-%m-%d")
-  script_nombre = os.path.basename(sys.argv[0])
-  
-  # recoger los argumentos
-  parser = argparse.ArgumentParser(
-                      prog=script_nombre,
-                      description='Lee los datos de Toggl para descargarlos en un fichero CSV')
-  parser.add_argument('-v', 
-                      '--version', 
-                      help="Version", 
-                      action='version', 
-                      version='%(prog)s 0.0.1')
-  parser.add_argument('-i', 
-                      '--inicio', 
-                      help="Fecha de INICIO en formato AAAA-MM-DD (por defecto hoy \"%(const)s\")", 
-                      required=True,
-                      nargs='?',
-                      const=hoy_cadena, 
-                      type=lambda s: datetime.strptime(s, "%Y-%m-%d"))
-  parser.add_argument('-f', 
-                      '--fin', 
-                      help="Fecha de FIN en formato AAAA-MM-DD (por defecto hoy \"%(const)s\")", 
-                      required=True,
-                      nargs='?',
-                      const=hoy_cadena, 
-                      type=lambda s: datetime.strptime(s, "%Y-%m-%d"))
-  parser.add_argument('-u',
-                      '--usuario',
-                      help="Usuario redmine para asignar los tiempos cargados", 
-                      required=True),
-  parser.add_argument('-e',
-                      '--entorno',
-                      help="Entorno de Desarrollo (DES) o Producción (PRO) (por defecto Desarrollo \"%(const)s\")", 
-                      required=True,
-                      nargs='?',
-                      choices=['des', 'pro'],
-                      const='des'
-                      )
 
-  try:
-      # extraer argumentos
-      args = parser.parse_args()
-      
-      # extraer los entornos
-      entornos = get_entornos()
-      
-      api_key_toggl = entornos[args.entorno]["API_KEY_TOGGL"]
-      api_key_redmine = entornos[args.entorno]["API_KEY_REDMINE"]
-      
-  except: #argparse.ArgumentError or SystemExit:
-      parser.print_help()
-      exit(1)
-  # Argumentos
-  print(f'Argumentos de \"{script_nombre}\": {args.inicio.strftime("%Y-%m-%d")}, {args.fin.strftime("%Y-%m-%d")}, {args.usuario}, {args.entorno}')
-  
-  # Example usage of the loaded entornos
-  print(f"API_KEY_TOGGL: {api_key_toggl}")
-  print(f"API_KEY_REDMINE: {api_key_redmine}")
+def main():
+    # variables generales
+    hoy_cadena = datetime.today().strftime("%Y-%m-%d")
+    script_nombre = os.path.basename(sys.argv[0])
+    
+    # recoger los argumentos
+    parser = argparse.ArgumentParser(
+                        prog=script_nombre,
+                        description='Lee los datos de Toggl para descargarlos en un fichero CSV')
+    parser.add_argument('-v', 
+                        '--version', 
+                        help="Version", 
+                        action='version', 
+                        version='%(prog)s 0.0.1')
+    parser.add_argument('-i', 
+                        '--inicio', 
+                        help="Fecha de INICIO en formato AAAA-MM-DD (por defecto hoy \"%(const)s\")", 
+                        required=True,
+                        nargs='?',
+                        const=hoy_cadena, 
+                        type=lambda s: datetime.strptime(s, "%Y-%m-%d"))
+    parser.add_argument('-f', 
+                        '--fin', 
+                        help="Fecha de FIN en formato AAAA-MM-DD (por defecto hoy \"%(const)s\")", 
+                        required=True,
+                        nargs='?',
+                        const=hoy_cadena, 
+                        type=lambda s: datetime.strptime(s, "%Y-%m-%d"))
+    parser.add_argument('-u',
+                        '--usuario',
+                        help="Usuario redmine para asignar los tiempos cargados", 
+                        required=True),
+    parser.add_argument('-e',
+                        '--entorno',
+                        help="Entorno de Desarrollo (DES) o Producción (PRO) (por defecto Desarrollo \"%(const)s\")", 
+                        required=True,
+                        nargs='?',
+                        choices=['des', 'pro'],
+                        const='des'
+                        )
+    try:
+        # extraer argumentos
+        args = parser.parse_args()
+        
+        # extraer los entornos
+        entornos = get_entornos()
+        
+        api_key_toggl = entornos[args.entorno]["API_KEY_TOGGL"]
+        api_key_redmine = entornos[args.entorno]["API_KEY_REDMINE"]
+        
+    except: #argparse.ArgumentError or SystemExit:
+        parser.print_help()
+        exit(1)
+    # Argumentos
+    print(f'Argumentos de \"{script_nombre}\": {args.inicio.strftime("%Y-%m-%d")}, {args.fin.strftime("%Y-%m-%d")}, {args.usuario}, {args.entorno}')
+    
+    # Example usage of the loaded entornos
+    print(f"API_KEY_TOGGL: {api_key_toggl}")
+    print(f"API_KEY_REDMINE: {api_key_redmine}")
 
 """
 fecha_inicio = "2024-02-25"
